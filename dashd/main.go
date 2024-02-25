423812556cb9c5db69bd554097ed753371cbcce0
bluestar_ <canikissfemboy@gmail.com>
2024-02-25 15:15:00 +0700

    little change

--- dashd/main.go
+++ dashd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -12,6 +13,9 @@ import (
 	"github.com/shirou/gopsutil/host"
 	"github.com/shirou/gopsutil/mem"
 )
+
+//go:embed dashw/dist/index.html
+var content embed.FS
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
@@ -72,12 +76,22 @@ func handleSystemInfo(conn *websocket.Conn) {
 			return
 		}
 
-		time.Sleep(1 * time.Second)
+		time.Sleep(500 * time.Millisecond)
 	}
 }
 
 func main() {
 	log.Println("Started.")
+	log.Println("http://localhost:4480/")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data, err := content.ReadFile("dashw/dist/index.html")
+		if err != nil {
+			http.Error(w, "could not read file", http.StatusInternalServerError)
+			return
+		}
+		w.Write(data)
+	})
 
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		conn, err := upgrader.Upgrade(w, r, nil)
